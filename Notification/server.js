4c1a15f0ba8ee68211e75675793d4a9817ca0809
Diogo Torrinhas <59964065+DiogoTorrinhas@users.noreply.github.com>
2023-03-06 17:37:58 +0000

    post and get

---
+++ Notification/server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+const sqlite3 = require("sqlite3").verbose();
+
+const app = express();
+
+// middleware to parse JSON and urlencoded request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set up the SQLite3 database connection
+const db = new sqlite3.Database('./notifications.db', (error) => {
+  if (error) {
+    console.error('Error connecting to database:', error.message);
+  } else {
+    console.log('Database connected successfully');
+  }
+});
+
+// Create the notifications table in the database if it doesn't exist
+db.run(
+  `CREATE TABLE IF NOT EXISTS notifications (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    subject TEXT NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )`,
+  (error) => {
+    if (error) {
+      console.error('Error creating notifications table:', error.message);
+    } else {
+      console.log('Notifications table created successfully');
+    }
+  }
+);
+
+// Set SendGrid API key
+sgMail.setApiKey('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');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+// API endpoint to send email
+app.post('/send-email', async (req, res) => {
+  const { to, subject, message } = req.body;
+
+  // Check if required fields are missing
+  if (!to || !subject || !message) {
+    return res.status(400).send('Missing required fields');
+  }
+
+  const email = {
+    to,
+    from: 'eventfinderteste@gmail.com',
+    subject,
+    text: message,
+  };
+
+  db.run(
+    `INSERT INTO notifications (email, subject, message)
+    VALUES (?, ?, ?)`,
+    [to, subject, message],
+    (error) => {
+      if (error) {
+        console.error('Error inseerting:', error.message);
+      } else {
+        console.log('Inserted');
+      }
+    }
+  );
+
+  try {
+    await sgMail.send(email);
+    return res.status(200).send('Email sent successfully');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Error sending email');
+  }
+
+});
+
+
+app.get('/send-email', (req, res) => {
+  // Retrieve all notifications from the database
+  db.all(`SELECT * FROM notifications`, (error, rows) => {
+    if (error) {
+      console.error('Error retrieving notifications from database:', error.message);
+      res.status(500).send('Error retrieving notifications');
+    } else {
+      res.status(200).json(rows);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
