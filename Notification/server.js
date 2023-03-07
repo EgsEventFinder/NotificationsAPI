9d29614185e99bfb7457e47b4c7bb16b74622f4c
Diogo Torrinhas <59964065+DiogoTorrinhas@users.noreply.github.com>
2023-03-07 19:21:07 +0000

    different types of notifications

--- Notification/server.js
+++ Notification/server.js
@@ -39,30 +39,57 @@ db.run(
 // Set SendGrid API key
 sgMail.setApiKey('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');
 
-// const emailTemplates = {
-//   [emailTypes.BUY_TICKET]: {
-//     subject: 'Thank you for buying a ticket!',
-//     text: 'Dear {{name}},\n\nThank you for buying a ticket for our event. We look forward to seeing you there!'
-//   },
-//   [emailTypes.CANCEL_EVENT]: {
-//     subject: 'Event cancellation',
-//     text: 'Dear {{name}},\n\nWe regret to inform you that our event has been cancelled. We apologize for any inconvenience this may cause.'
-//   }
-// };
-
-
 // API endpoint to get index to test 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index2.html')
 })
 
 // API endpoint to send email
 app.post('/notification', async (req, res) => {
-  const { to, subject, message } = req.body;
 
-  // Check if required fields are missing
-  if (!to || !subject || !message) {
-    return res.status(400).send('Missing required fields');
+  // const { to, subject, message } = req.body;
+
+  // // Check if required fields are missing                        //code for the situation that we want to write our custom subject and our custom message
+  // if (!to || !subject || !message) {
+  //   return res.status(400).send('Missing required fields');
+  // }
+
+  const { to, type } = req.body;
+  let subject, message;
+
+  //different types of notifications
+
+  switch (type) {
+    case 'ticket_buy':
+      subject = 'Ticket purchase confirmation';
+      message = 'Thank you for your purchase. Your ticket(s) have been confirmed.';
+      break;
+    case 'event_cancelation':
+      subject = 'Event cancellation';
+      message = 'We regret to inform you that the event, [Event Name] you registered for has been canceled.';
+      break;
+    case 'ticket_refund':
+      subject = 'Ticket refund';
+      message = 'Your ticket has been refunded in full.';
+      break;
+    case 'event_announcement':
+      subject = 'Event Announcement';
+      message = 'We are excited to announce a new event, [Event Name], and we hope you will be able to attend.';
+      break;
+    case 'special_offer':
+      subject = 'Special Offer';
+      message = 'We would like to offer you a special discount on your next ticket purchase.';
+      break;
+    case 'thank_you_email':
+      subject = 'Thank you for attending';
+      message = 'We wanted to take a moment to thank you for attending [Event Name]. We hope you had a great time and enjoyed the Event';
+      break;
+    case 'schedule_change':
+      subject = 'Schedule Change';
+      message = 'We wanted to let you know that there has been a change to the schedule for [Event Name]. We apologize for any inconvenience this may cause and hope that you can still attend. If you are unable to attend the event due to this change, please contact us for a refund.';
+      break;
+    default:
+      return res.status(400).json({ message: 'Invalid notification type' });
   }
 
   const email = {
@@ -96,7 +123,6 @@ app.post('/notification', async (req, res) => {
     console.error(error);
     return res.status(500).send('Error sending email');
   }
-
 });
 
 // API endpoint to get all notifications
